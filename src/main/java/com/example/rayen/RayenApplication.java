9d89066bf9b49729fdd73bcdb46ea13dc224d56d
rayen-khalfaoui <rayen.khalfaoui@esprit.tn>
2024-07-17 16:32:18 +0200

    efezfqf

---
+++ src/main/java/com/example/rayen/RayenApplication.java
@@ -0,0 +1,13 @@
+package com.example.rayen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RayenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RayenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rayen/RayenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rayen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RayenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
